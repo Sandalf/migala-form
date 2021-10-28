f793520bd3d42925fad0445af8774ed62e54bff6
Luis Sandoval <alfsand97@gmail.com>
2021-10-28 15:32:28 -0600

    fix: Reemplazar API de codigos postales por Geocodes

--- src/model/ZipCodes/ZipCodesResponse.ts
+++ src/model/ZipCodes/ZipCodesResponse.ts
@@ -1,14 +1,10 @@
+export interface ZipCode {
+    cp: String
+    colonia: String
+    municipio: String
+    estado: String
+}
+
 export interface ZipCodesResponse {
-    0: string;
-    IdEnt: string;
-    1: string;
-    Entidad: string;
-    2: string;
-    Ciudad: string;
-    3: string;
-    Municipio: string;
-    4: string;
-    Colonia: string;
-    5: string;
-    CP: string;
+    postalCodes: Array<ZipCode>
 }
